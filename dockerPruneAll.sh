e4e67557382114fff3f3821dba08a0ed1d6356db
Marcus Vinícius Braga Alcântara <vinicius.alcantara@somosagility.com.br>
2021-04-16 04:42:55 +0000

    Update dockerPruneAll.sh

--- dockerPruneAll.sh
+++ dockerPruneAll.sh
@@ -3,7 +3,7 @@
 #### VARs ####
 DOCKER="$(which docker)";
 DISK_USAGE_ALERT=85;
-DISK_USAGE_CURRENT=$(df -h / | tail -n1 | cut -d " " -f 12 | cut -c 1-2);
+DISK_USAGE_CURRENT=$(df -h / --output=pcent | tail -n1 | sed 's/ //g' | sed 's/%//');
 ##############
 
 if [ $DISK_USAGE_CURRENT -ge $DISK_USAGE_ALERT 2> /dev/null ]; then
